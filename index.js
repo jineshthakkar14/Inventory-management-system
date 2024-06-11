26ab3d61f3f52aebcb734b9f6853f2b5098bc6fb
jineshthakkar14 <jinesh.22110264@viit.ac.in>
2024-06-11 18:31:05 +0530

    Added the functionallity of login and signup, added the business logic and added the api for it.
    In signup, first we are comparing password and confirm password. Then we are checking in the database that user exists or not. Then we are hashing the password using bcrypt and saving the hashed password in database.
    
    In login, we are first checking in the database if user exists or not. Then we are checking the password with the hashed password in the database using bcrypt.compare() and then we are creating token using json web token.

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 const database = require("./config/database");
+const userRoutes = require("./routes/user");
+const cookieParser = require("cookie-parser");
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/v1/auth", userRoutes);
 
 database.connect();
 
